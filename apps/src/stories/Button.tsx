6f27739b0f52531515a4617f457480e25c79b845
Pedro Paulo <prado.tecnology@gmail.com>
2025-01-29 11:11:13 -0300

    Feature: First alter in components

--- apps/src/stories/Button.tsx
+++ apps/src/stories/Button.tsx
@@ -1,29 +1,22 @@
-import React from 'react';
-
 import './button.css';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
+  variant?: 'primary' | 'secondary' | 'tertiary';
   backgroundColor?: string;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
 }
 
 /** Primary UI component for user interaction */
 export const Button = ({
-  primary = false,
+  variant = 'primary',
   size = 'medium',
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = variant ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
